2197d414945d728326d736908715097af3e3eff0
Danilo Santos Oliveira <eng.danilosantosoliveira@gmail.com>
2026-06-30 19:06:41 +0000

    sd

--- leader_controller/pure_pursuit.py
+++ leader_controller/pure_pursuit.py
@@ -207,7 +207,7 @@ class pure_pursuit(Node):
           enable = 0.0
 
         # publishing commands
-        self.desired_steering = 0.4  # Ensure steering is in radians
+        #self.desired_steering = 0.4  # Ensure steering is in radians
         # speed_command = 0.2  # Set a constant speed command
         self.nav_command(enable, speed_command)
         self.publish_steering_current()
